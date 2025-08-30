03b94b8f00481a16f3fde53b38928803be39616a
Alex Nypels <nypelsa@gmail.com>
2025-08-30 14:10:08 +0100

    TKW-81 - fix: adjust background color calculation in PriorityTaskCard and PriorityTaskList

--- features/dashboard/screens/home/components/PriorityTaskList.tsx
+++ features/dashboard/screens/home/components/PriorityTaskList.tsx
@@ -14,7 +14,7 @@ const getBgColor = (id: string | number) => {
     Math.abs(
       [...id.toString()].reduce((acc, char) => acc + char.charCodeAt(0), 0),
     ) % 10;
-  return index;
+  return index + 1;
 };
 
 const PriorityTaskList = () => {
